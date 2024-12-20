f21cf4a79e40e786ddccaf15519bd6f0c5add411
Bruno Galiati <brunop.galiati@gmail.com>
2024-12-19 21:44:28 -0300

    refresh google sheets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import getSheetData from "@/services/sheets";
 import { MainContent } from "@/components/MainContent";
 
+export const revalidate = 300; // sempre revalida
+
 export default async function Home() {
   const { presidents, indicators } = await getSheetData();
 
